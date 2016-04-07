31c2d7dfe676726eae35d7aaed9d740ca453272d
Diogo Ferreira <defer@cyngn.com>
2016-04-07 15:11:28 +0100

    mt6735: Add an RGB configuration sys attr driver for videox/corr10
    Adds the RGB configuration node which leverages the gamma subsystem to
    perform color correction.
    
    The new node takes rgb triplets that range from 0-2000 each, computes
    the gamma registers and dispatches them to the gamma correction
    subsystem.
    
    Change-Id: Iac9d3cbd4f423ccfffb8d665c29cfd251767a398

--- build-kernel.sh
+++ build-kernel.sh
@@ -8,7 +8,7 @@ export HW_PROJECT="M1_K109_DT600"
 
 #Echo actual vars
 echo "We are actually building for $HW_PROJECT with $CROSS_COMPILE"
-make len6735_66_m0_M1_K109_DT600_defconfig
+make runbo_m1_defconfig
 #make clean
 #export MAKEFLAGS=-j9
 make -j9
--- clean-kernel.sh
+++ clean-kernel.sh
@@ -8,6 +8,6 @@ export HW_PROJECT="M1_K109_DT600"
 
 #Echo actual vars
 echo "We are actually clean for $HW_PROJECT with $CROSS_COMPILE"
-make len6735_66_m0_M1_K109_DT600_defconfig
+make runbo_m1_defconfig
 make clean
 
